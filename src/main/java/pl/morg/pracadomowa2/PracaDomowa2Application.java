cf316f2fc6de6e11b2963eaebfd74bdefb7968c6
przemyslaw.luczak <przemyslaw.luczak.92@gmail.com>
2020-10-27 21:53:26 +0100

    initial commit

---
+++ src/main/java/pl/morg/pracadomowa2/PracaDomowa2Application.java
@@ -0,0 +1,13 @@
+package pl.morg.pracadomowa2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracaDomowa2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracaDomowa2Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/morg/pracadomowa2/PracaDomowa2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.morg.pracadomowa2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracaDomowa2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
